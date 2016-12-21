d17346159c9b6ffef7d3fd45ebe0bfa6ae596b98
Jan_ <Jan_@JAN-PC>
2016-12-22 00:24:08 +0100

    reader fail

--- Sudoku/Sudoku/Main.cpp
+++ Sudoku/Sudoku/Main.cpp
@@ -3,7 +3,11 @@
 #include <vector>
 #include <string>
 
-int main
+int main()
 {
-
+	Reader r("C:\\Users\\Jan_\\Desktop\\Sudoku1.txt");
+	for (const string& line : r.read())
+	{
+		cout << line << endl;
+	}
 }
