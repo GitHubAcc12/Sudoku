d406388f7632078cace79df16db43db984442b98
Jan_ <Jan_@JAN-PC>
2016-12-22 13:01:44 +0100

    Tests ausgelagert (keine Ahnung ob sinnvoll)

--- Sudoku/Sudoku/Main.cpp
+++ Sudoku/Sudoku/Main.cpp
@@ -1,26 +1,16 @@
 #include "Reader.h"
 #include "Writer.h"
+#include "IOTests.h"
 #include <iostream>
 #include <vector>
 #include <string>
 
+
+
+
+
+
 int main()
 {
-	auto path = "Sudoku1.txt";
-	Reader r(path);
-	const auto& v = r.read();
-	for (const string& line : v)
-	{
-		cout << line << endl;
-	}
-	path = "output.txt";
-	Writer w(path);
-	w.write(v);
-	Reader r2(path);
-	cout << "-----------------------------------------------" << endl;
-	for (const string& line : r2.read())
-	{
-		cout << line << endl;
-	}
-	
+	io_test();
 }
