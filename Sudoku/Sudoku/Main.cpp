c5a63686a661ba0c9f75c0271b4b4f8049e26690
GitHubAcc12 <jan.kretschmann@rwth-aachen.de>
2017-12-30 13:44:02 +0100

    Dateiauswahl

--- Sudoku/Sudoku/Main.cpp
+++ Sudoku/Sudoku/Main.cpp
@@ -7,15 +7,25 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-	auto file = "Sudoku1.txt";
-	Reader r(file);
+	string file;
+	if (argc == 2)
+	{
+		file = argv[1];
+	} 
+	else
+	{
+		cout << "Inputfile: ";
+		cin >> file;
+	}
+
+	Reader r(file.c_str());
 	auto& v = r.read();
 	Sudoku s(v);
 	cout << s.tostring() << endl;
 	s.solve();
 	file = "output.txt";
-	Writer w(file);
+	Writer w(file.c_str());
 	w.write(s.sudoku);
 }
